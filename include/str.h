c64c67641160bef15668c069c98712bb8e9fe3f5
bmoreira <bmoreira@student.42sp.org.br>
2026-02-08 01:05:05 -0300

    added split le

--- include/str.h
+++ include/str.h
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 22:42:31 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/02/08 00:43:45 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/02/08 01:03:57 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include "mem.h"
 
+int     ft_split_len(char **split);
 char	*ft_reverse_split(char **split, int size, char c);
 char	**ft_split_free(char **split);
 char	**ft_split(const char *s, char c);
