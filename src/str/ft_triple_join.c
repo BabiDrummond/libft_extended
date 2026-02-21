214d66dbebe518004290c29944d1eeafa2443c20
bmoreira <bmoreira@student.42sp.org.br>
2026-02-21 19:37:48 -0300

    refactoring ft_triple_join

--- src/str/ft_triple_join.c
+++ src/str/ft_triple_join.c
@@ -6,7 +6,7 @@
 /*   By: bmoreira <bmoreira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 19:25:33 by bmoreira          #+#    #+#             */
-/*   Updated: 2026/02/19 19:28:34 by bmoreira         ###   ########.fr       */
+/*   Updated: 2026/02/21 19:16:20 by bmoreira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 
 char	*ft_triple_join(char *s1, char *s2, char *s3)
 {
-   	char	*new_str;
-	char	*temp;
+	char	*new;
+	int		i;
+	int		j;
 
-	new_str = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1 + ft_strlen(s3) + 1, sizeof(char));
-	temp = new_str;
-	if (!new_str)
+	i = 0;
+	j = 0;
+	new = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + ft_strlen(s3) + 1,
+			sizeof(char));
+	if (!new)
 		return (NULL);
-	while (*s1)
-		*new_str++ = *s1++;
-	while (*s2)
-		*new_str++ = *s2++;
-    while (*s3)
-		*new_str++ = *s3++;
-	return (temp);
+	while (s1[j])
+		new[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		new[i++] = s2[j++];
+	j = 0;
+	while (s3[j])
+		new[i++] = s3[j++];
+	return (new);
 }
